8f155fd86c7f81747e04a4ddf4786b072b1f0996
MAllena <MAllena@rosariobus.com.ar>
2022-08-04 16:51:11 -0300

    login component y authService

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../pages/interfaces/auth.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  constructor( private http: HttpClient) { }
+
+  login(){
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+  }
+}
